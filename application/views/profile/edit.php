f3a5572140eb8738bd5f58b0b3de98cf00e6deb2
suresh07 <soori_rozee@yahoo.com>
2018-12-04 16:15:24 +0530

    Error message displayed if mandatory field is missing

--- application/views/profile/edit.php
+++ application/views/profile/edit.php
@@ -1,5 +1,5 @@
 <?php
-    $admin = true;
+    $admin = false;
     $rand = rand();
 
     if(file_exists(PHY_AVATAR_URL . $data['id'] . '.jpg'))
@@ -11,7 +11,7 @@
     <h1>Edit Bio-Data</h1>
     <div class="row">
         <div class="col-md-9 main mainform">
-            <form action="<?=BASE_URL?>profile/update" method="post" id="addOrUpdateForm">
+            <form action="<?=BASE_URL?>profile/update" method="post" id="editForm">
                 <fieldset class="form-control">
                     <legend>Profile</legend>
                     <div class="form-group field">
@@ -34,14 +34,14 @@
                             <label for="profile-name-first" class="col-md-2 col-form-label">First name</label>
                             <div class="col-md-10">
                                 <input required class="form-control" type="text" name="profile-name-first" id="profile-name-first" value="<?=isset($data['profile']['name']['first']) ? $data['profile']['name']['first'] : ''?>"/>
-                                <small id="profile-name-first-help" class="form-text text-muted">Name is mandatory</small>
+                                <small id="profile-name-first-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
                             <label for="profile-name-last" class="col-md-2 col-form-label">Last name</label>
                             <div class="col-md-10">
                                 <input required class="form-control" type="text" name="profile-name-last" id="profile-name-last" value="<?=isset($data['profile']['name']['last']) ? $data['profile']['name']['last'] : ''?>"/>
-                                <small id="profile-name-last-help" class="form-text text-muted">Name is mandatory</small>
+                                <small id="profile-name-last-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
@@ -57,7 +57,7 @@
                             <label for="profile-birthDate" class="col-md-2 col-form-label">Date of Birth</label>
                             <div class="col-md-10">
                                 <input required pattern="\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])" placeholder="yyyy-mm-dd" class="form-control datePicker" type="text" name="profile-birthDate" id="profile-birthDate" value="<?=isset($data['profile']['birthDate']) ? $data['profile']['birthDate']:''?>" />
-                                <!-- <small id="profile-birthDate-help" class="form-text text-muted">Date is mandatory</small> -->
+                                <small id="profile-birthDate-help" class="form-text text-muted">Date of birth is mandatory</small>
                             </div>
                         </div>
                         <?php if($admin) { ?>
@@ -65,7 +65,7 @@
                             <label for="profile-deathDate" class="col-md-2 col-form-label">Date of Death</label>
                             <div class="col-md-10">
                                 <input pattern="\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])" placeholder="yyyy-mm-dd" class="form-control datePicker" type="text" name="profile-deathDate" id="profile-deathDate" value="<?=isset($data['profile']['deathDate']) ? $data['profile']['deathDate']:''?>" />
-                                <!-- <small id="profile-deathDate-help" class="form-text text-muted">Date is mandatory</small> -->
+                                <!-- <small id="profile-deathDate-help" class="form-text text-muted">Date of death</small> -->
                             </div>
                         </div>
                         <?php } else { ?>
@@ -75,18 +75,20 @@
                             <label for="profile-degree" class="col-md-2 col-form-label">Degree</label>
                             <div class="col-md-10">
                                 <input required class="form-control" type="text" name="profile-degree" id="profile-degree" value="<?=isset($data['profile']['degree']) ? $data['profile']['degree'] : ''?>" />
+                                <small id="profile-degree-help" class="form-text text-muted">Degree is required</small>
                             </div>
                         </div>
                         <div class="row subField">
                             <label for="profile-honours" class="col-md-2 col-form-label">Honours</label>
                             <div class="col-md-10">
-                                <input required class="form-control" type="text" name="profile-honours" id="profile-honours" value="<?=isset($data['profile']['honours']) ? $data['profile']['honours'] : ''?>" />
+                                <input class="form-control" type="text" name="profile-honours" id="profile-honours" value="<?=isset($data['profile']['honours']) ? $data['profile']['honours'] : ''?>" />
                             </div>
                         </div>
                         <div class="row subField">
                             <label for="profile-specialization" class="col-md-2 col-form-label">Specialization</label>
                             <div class="col-md-10">
                                 <input required class="form-control" type="text" name="profile-specialization" id="profile-specialization" value="<?=isset($data['profile']['specialization']) ? $data['profile']['specialization'] : ''?>" />
+                                <small id="profile-degree-help" class="form-text text-muted">Degree is required</small>
                             </div>
                         </div>
                     </div>
@@ -139,22 +141,22 @@
                         <div class="row subField">
                             <label for="contact-address" class="col-md-2 col-form-label">Address</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="text" name="contact-address" id="contact-address" value="<?=isset($data['contact']['address']) ? $data['contact']['address'] : ''?>"/>
-                                <!-- <small id="contact-address-help" class="form-text text-muted">Mandatory</small> -->
+                                <input required class="form-control" type="text" name="contact-address" id="contact-address" value="<?=isset($data['contact']['address']) ? $data['contact']['address'] : ''?>"/>
+                                <small id="contact-address-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
                             <label for="contact-city" class="col-md-2 col-form-label">City</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="text" name="contact-city" id="contact-city" value="<?=isset($data['contact']['city']) ? $data['contact']['city'] : ''?>"/>
-                                <!-- <small id="contact-city-help" class="form-text text-muted">Mandatory</small> -->
+                                <input required class="form-control" type="text" name="contact-city" id="contact-city" value="<?=isset($data['contact']['city']) ? $data['contact']['city'] : ''?>"/>
+                                <small id="contact-city-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
                             <label for="contact-state" class="col-md-2 col-form-label">State</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="text" name="contact-state" id="contact-state" value="<?=isset($data['contact']['state']) ? $data['contact']['state'] : ''?>"/>
-                                <!-- <small id="contact-state-help" class="form-text text-muted">Mandatory</small> -->
+                                <input required class="form-control" type="text" name="contact-state" id="contact-state" value="<?=isset($data['contact']['state']) ? $data['contact']['state'] : ''?>"/>
+                                <small id="contact-state-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <legend><small>Telephone</small></legend>
@@ -162,7 +164,7 @@
                             <label for="contact-telephone-office" class="col-md-2 col-form-label">Office</label>
                             <div class="col-md-10">
                                 <input class="form-control" type="text" name="contact-telephone-office" id="contact-telephone-office" value="<?=isset($data['contact']['telephone']['office']) ? $data['contact']['telephone']['office'] : ''?>"/>
-                                <!-- <small id="contact-telephone-office-help" class="form-text text-muted">Mandatory</small> -->
+                                <small id="contact-telephone-office-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
@@ -175,8 +177,8 @@
                         <div class="row subField">
                             <label for="contact-telephone-mobile" class="col-md-2 col-form-label">Mobile</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="text" name="contact-telephone-mobile" id="contact-telephone-mobile" value="<?=isset($data['contact']['telephone']['mobile']) ? $data['contact']['telephone']['mobile'] : ''?>"/>
-                                <!-- <small id="contact-telephone-mobile-help" class="form-text text-muted">Mandatory</small> -->
+                                <input required class="form-control" type="text" name="contact-telephone-mobile" id="contact-telephone-mobile" value="<?=isset($data['contact']['telephone']['mobile']) ? $data['contact']['telephone']['mobile'] : ''?>"/>
+                                <small id="contact-telephone-mobile-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
@@ -190,8 +192,8 @@
                         <div class="row subField">
                             <label for="contact-email-personal" class="col-md-2 col-form-label">Personal</label>
                             <div class="col-md-10">
-                                <input class="form-control" type="text" name="contact-email-personal" id="contact-email-personal" value="<?=isset($data['contact']['email']['personal']) ? $data['contact']['email']['personal'] : ''?>"/>
-                                <!-- <small id="contact-email-personal-help" class="form-text text-muted">Mandatory</small> -->
+                                <input required class="form-control" type="email" name="contact-email-personal" id="contact-email-personal" value="<?=isset($data['contact']['email']['personal']) ? $data['contact']['email']['personal'] : ''?>"/>
+                                <small id="contact-email-personal-help" class="form-text text-muted">Mandatory</small>
                             </div>
                         </div>
                         <div class="row subField">
@@ -262,6 +264,8 @@
         });
         
     }
+
+    formActions();
 
 
 </script>
--- public/js/common.js
+++ public/js/common.js
@@ -5,3 +5,99 @@ $(function(){
 		$('#mainNavBar .navbar-nav li:contains(Login)').hide();
 	}
 });
+
+function enableSubmitIfValid(){
+
+	var form = document.querySelector("#editForm");
+	if(form.checkValidity()) {
+	 
+	    $('#submit').prop('disabled', false).addClass('btn-primary');
+	}
+	else{
+		
+	    $('#submit').prop('disabled', true).removeClass('btn-primary');
+	}
+}
+function formActions(){
+
+	$('.dateTypeSelector input[name="dateType"]').on('change', function(){
+
+	    $('.dateSelector').addClass('hidden');
+	    $('.dateSelector input, .dateSelector select').prop('required', false);
+
+	    var targetSelector = '#' + $('.dateTypeSelector input[name="dateType"]:checked').val() + 'DateSelector';
+	    $(targetSelector).removeClass('hidden');
+	    $(targetSelector + ':not(.hidden) input').prop('required', true);
+	    $(targetSelector + ':not(.hidden) :not(.hidden) select').prop('required', true);
+	});
+
+	$('#hinduDateSelector .more i').on('click', function(){
+
+	    $(this).hide();
+	    $('#hinduSingleDateSelector').addClass('hidden');
+	    $('#hinduSingleDateSelector select').prop('required', false);
+	    $('#pooja-date-hindu').removeClass('hidden');
+	});
+
+	// Calculate hndu date and put value in pooja-data-hindu
+	$('#pooja-date-hindu-maasa, #pooja-date-hindu-paksha, #pooja-date-hindu-tithi').on('change', function(){
+
+	    var maasa = $('#pooja-date-hindu-maasa').val();
+	    var paksha = parseInt($('#pooja-date-hindu-paksha').val());
+	    var tithi = parseInt($('#pooja-date-hindu-tithi').val());
+
+	    tithi = (tithi % 15 == 0) ? tithi : ((paksha - 1) * 15) + tithi;
+	    if (tithi < 10) tithi = '0' + tithi;
+	    var hinduDate = maasa + '-' + tithi;
+
+	    if(hinduDate.match(/\d\d-\d\d/)) $('#pooja-date-hindu').val(hinduDate);
+	});
+
+	$('.dateTypeSelector input[name="dateType"]').trigger("change");
+
+	$('.mainform .datePickerReceipt').datepicker({
+
+	    format: "yyyy-mm-dd",
+	    todayHighlight: true,
+	    forceParse: false
+	});
+
+	$('.mainform .datePickerPooja').datepicker({
+
+	    format: "mm-dd",
+	    clearBtn: true,
+	    todayHighlight: true,
+	    multidate: true,
+	    multidateSeparator: ";",
+	    forceParse: false
+	});
+
+	$('#editForm input').on('change', function(){
+
+	    enableSubmitIfValid();
+	});
+
+	$('#editForm input').on('focusin', function(){
+
+	    $(this).addClass('validate');
+	    $('#' + $(this).attr('id') + '-help').show();
+	});
+
+	$('#editForm input').on('focusout', function(){
+
+	    if($(this).is(":valid"))
+	    	$('#' + $(this).attr('id') + '-help').hide();
+	});
+
+	$('#submitRegion').on('click, mouseover', function(){
+
+		$('#editForm input').each(function(){
+
+			if($(this).is(":invalid")) {
+		    
+		    	$(this).addClass('validate');
+		    	$('#' + $(this).attr('id') + '-help').show();
+		    }
+		});
+	});
+}

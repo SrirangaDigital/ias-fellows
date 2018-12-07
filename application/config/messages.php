91fad7d52e6e8925e4d236ec12567776ec8a01fa
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-12-07 15:34:33 +0530

    git messages included

--- application/config/messages.php
+++ application/config/messages.php
@@ -2,4 +2,8 @@
 
 // If messgages need to contain variables, put them as :foo and bind them using model::bindVariablesToString() method
 
+define('GIT_ADD_MSG', 'Details added');
+define('GIT_MOD_MSG', 'Details modifed');
+define('GIT_DEL_MSG', 'Details deleted');
+
 ?>
